3550d9f9cf3720c704c1029910fd71dc7c1113ec
shucheng <shucheng@bd-apaas.com>
2022-10-20 19:18:00 +0800

    v0.23.0

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -16,3 +16,8 @@ export GOARCH=ppc64le
 make binaries
 mkdir -p _output/linux/$GOARCH
 mv _output/nerdctl _output/linux/$GOARCH/nerdctl
+
+export GOARCH=mips64le
+make binaries
+mkdir -p _output/linux/$GOARCH
+mv _output/nerdctl _output/linux/$GOARCH/nerdctl
